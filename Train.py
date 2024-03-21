d838105af620bf0b236e34518014ceec5db5c60d
JoJohanse <97782983+JoJohanse@users.noreply.github.com>
2024-03-21 13:36:12 +0800

    Update Train.py

--- Train.py
+++ Train.py
@@ -2,9 +2,11 @@ import numpy as np
 import torch
 from MedicineRecommend import MedicineRecommend
 from torch.utils.data import DataLoader, TensorDataset, random_split
+import pandas as pd
 
 # 设置使用GPU
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
 
 # 加载数据
 '''
@@ -35,36 +37,61 @@ test_size = len(dataset) - train_size
 Train_dataset, Test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
 X_train, y_train = Train_dataset[:]
 X_test, y_test = Test_dataset[:]
-epoches = 100
-batch_size = 10
+# save
+# np.save('X_train.npy', X_train.cpu().numpy())
+# np.save('y_train.npy', y_train.cpu().numpy())
+# np.save('X_test.npy', X_test.cpu().numpy())
+# np.save('y_test.npy', y_test.cpu().numpy())
+# load
+# X_train = torch.from_numpy(np.load('X_train.npy')).float().to(device)
+# y_train = torch.from_numpy(np.load('y_train.npy')).float().to(device)
+# X_test = torch.from_numpy(np.load('X_test.npy')).float().to(device)
+# y_test = torch.from_numpy(np.load('y_test.npy')).float().to(device)
+
+epoches = 500
+batch_size = 100
 
 # 编译模型
 model = MedicineRecommend(768, 500, 360,293)
 model.compile_model()
 model.to(device)
 
-# # 训练模型
-# model.train_model(X_train, y_train, epoches, batch_size)
-# # 保存模型
-# model.save('save/model.pth')
+# 训练模型
+# model.train_model(X_train, y_train,epoches, batch_size)
+model.load_model('save/model.pth')
+model.evaluate_model(X_test[:10], y_test[:10])
+model.save_model('save/model.pth')
+# # 加载模型
+# model.load_model('model.pth')
 
-# 加载模型
-def load(self, path):
-    self.model.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
-load(model, 'save/model.pth')
+# # 预测前5个病人的带药情况
+# rusults = model.predict_model(X_test[:5])
+# # 根据输出，匹配药品名
+# # 加载药品名
+# drug_names = pd.read_csv('drug_list.csv', header=None,encoding='GB2312').values
 
-# 预测
-rusults = model.predict_model(X_test)
-print("病人",1,"的预测结果为：")
-print(rusults[1])
-print("test",1,"的真实结果为：")
-print(y_test[1])
-# 计算准确率
-correct = 0
-total = 0
-for i in range(10):
-    for j in range(len(rusults[i])):
-        if rusults[i][j] == y_test[i][j]:
-            correct += 1
-        total += 1
-print('Accuracy: {:.2f}%'.format(100 * correct / total))
+# # 输出结果
+# for i in range(len(rusults)):
+#     print('病人{}:'.format(i))
+#     print('预测带药：')
+#     for j in range(len(rusults[i])):
+#         if rusults[i][j] == 1:
+#             print(drug_names[j])
+#     print()
+# # 与测试集结果对比
+#     test_y = y_test.cpu().numpy()
+#     for k in range(len(y_test[i])):
+
+#         if y_test[i][k] == 1:
+#             print(drug_names[k])
+#     correct = 0
+#     total = 0
+#     for j in range(len(rusults[i])):
+#         if y_test[i][j] == 1:
+#             if rusults[i][j] == 1:
+#                 correct += 1
+#             total += 1
+#     if total == 0:
+#         print('实际带药数为0，无法计算准确率')
+#         continue
+#     print('Accuracy: {:.2f}%'.format(100 * correct / total))
