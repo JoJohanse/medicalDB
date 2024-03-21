890140a3d213009e5ba9edb3ac77769dd80e28f0
JoJohanse <97782983+JoJohanse@users.noreply.github.com>
2024-03-21 13:35:28 +0800

    Update MedicineRecommend.py

--- MedicineRecommend.py
+++ MedicineRecommend.py
@@ -1,8 +1,9 @@
 import torch
 from torch import nn
 from torch.optim import Adam
+import pandas as pd
 
-
+drug_names = pd.read_csv('drug_list.csv', header=None,encoding='GB2312').values
 class MedicineRecommend(nn.Module):
     def __init__(self, input_dim, hidden_dim1, hidden_dim2, output_dim):
         super(MedicineRecommend, self).__init__()
@@ -30,44 +31,58 @@ class MedicineRecommend(nn.Module):
                 outputs = self.model(X_batch)
                 loss = self.criterion(outputs, y_batch)
                 # 打印loss
-                if i % 100 == 0:
+                if i % 1000 == 0:
                     print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch + 1, epochs, i + 1, X_train.size(0), loss.item()))
                 # 反向传播
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
-    def evaluate_model(self, X_test, y_test):
+    def evaluate_model(self,X_test, y_test):
         with torch.no_grad():
-            outputs = self.model(X_test)
-            loss = self.criterion(outputs, y_test)
-            # 计算准确率
-            for i in range(len(outputs)):
-                for j in range(len(outputs[i])):
-                    if outputs[i][j] > 0.5:
-                        outputs[i][j] = 1
-                    else:
-                        outputs[i][j] = 0
-            correct = 0
-            total = 0
-            for i in range(len(outputs)):
-                for j in range(len(outputs[i])):
-                    if outputs[i][j] == y_test[i][j]:
-                        correct += 1
-                    total += 1
-            print('Accuracy: {:.2f}%'.format(100 * correct / total))
+            results,drug_lists = self.predict_model(X_test)
+            loss = self.criterion(results, y_test)
+            for i in range(len(results)):
+                print('病人{}:'.format(i))
+                print('预测带药：')
+                for j in range(len(results[i])):
+                    if results[i][j] == 1:
+                        print(drug_lists[i][j])
+                # 与测试集结果对比
+                print('实际带药：')
+                for k in range(len(y_test[i])):
+                    if y_test[i][k] == 1:
+                        print(drug_names[k])
+                correct = 0
+                total = 0
+                for j in range(len(results[i])):
+                    if y_test[i][j] == 1:
+                        if results[i][j] == 1:
+                            correct += 1
+                        total += 1
+                if total == 0:
+                    continue
+                print('Accuracy: {:.2f}%'.format(100 * correct / total))
             return loss.item()
-
     def predict_model(self, X):
         with torch.no_grad():
             outputs = self.model(X)
+            drug_lists = []
             for i in range(len(outputs)):
                 for j in range(len(outputs[i])):
                     if outputs[i][j] > 0.5:
                         outputs[i][j] = 1
                     else:
                         outputs[i][j] = 0
-            return outputs
+            for i in range(len(outputs)):
+                drug_list = []
+                for j in range(len(outputs[i])):
+                    if outputs[i][j] == 1:
+                        drug_list.append(drug_names[j])
+                    elif outputs[i][j] == 0:
+                        drug_list.append('')
+                drug_lists.append(drug_list)
+            return outputs,drug_lists
 
     def save_model(self, path):
         torch.save(self.model.state_dict(), path)
